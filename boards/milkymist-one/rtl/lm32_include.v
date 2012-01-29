cdf3fc78a7104036770d1c290a650efbb5e7e2a9
Yann Sionneau <yann@minet.net>
2012-01-29 22:47:42 +0100

    Fix definition of TLB CSR index

--- boards/milkymist-one/rtl/lm32_include.v
+++ boards/milkymist-one/rtl/lm32_include.v
@@ -280,10 +280,10 @@
 `define LM32_CSR_WP1                    `LM32_CSR_WIDTH'h19
 `define LM32_CSR_WP2                    `LM32_CSR_WIDTH'h1a
 `define LM32_CSR_WP3                    `LM32_CSR_WIDTH'h1b
-`define LM32_CSR_DTLB_FLUSH		`LM32_CSR_WIDTH'h1c
-`define LM32_CSR_DTLB_VADDRESS		`LM32_CSR_WIDTH'h1d
-`define LM32_CSR_DTLB_PADDRESS		`LM32_CSR_WIDTH'h1e
 `endif 
+`define LM32_CSR_TLB_FLUSH		`LM32_CSR_WIDTH'h1c
+`define LM32_CSR_TLB_VADDRESS		`LM32_CSR_WIDTH'h1d
+`define LM32_CSR_TLB_PADDRESS		`LM32_CSR_WIDTH'h1e
 
 // Values for WPC CSR
 `define LM32_WPC_C_RNG                  1:0
