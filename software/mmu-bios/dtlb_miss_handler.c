70b09662cb06f104a2d9e37da57963bbfcee6cb8
Yann Sionneau <yann@minet.net>
2012-06-01 22:54:18 +0200

    Add a real DTLB miss exception handler

--- software/mmu-bios/dtlb_miss_handler.c
+++ software/mmu-bios/dtlb_miss_handler.c
@@ -1,6 +1,26 @@
 #include <hal/mmu.h>
+#include <base/mmu.h>
 
 void dtlb_miss_handler(void)
 {
-	mmu_dtlb_map(0x44000000, 0x44003000);
+	unsigned int vaddr, paddr;
+
+	// retrieve virtual address which caused the page fault
+	asm volatile("rcsr %0, dtlbma" : "=r"(vaddr) :: );
+
+	/*
+	* check if there is an existing mapping for that virtual address
+	* if yes: refill the DTLB with it
+	* if not: we panic() !
+	*/
+	paddr = get_mmu_mapping_for(vaddr);
+	if (paddr == A_BAD_ADDR)
+	{
+		puts("Unrecoverable page fault !");
+		panic();
+	}
+
+	printf("Refilling DTLB with mapping 0x%08X->0x%08X\n", vaddr, paddr);
+	mmu_dtlb_map(vaddr, paddr);
+
 }
